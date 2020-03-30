80f81d1519a8e4389a30880869b47f087c9454ac
Erick Reis <erickcorreareis@gmail.com>
2020-03-30 02:23:45 -0300

    [Frontend] Adicionando Linter

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi';
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
 
-import api from '../../services/api';
+import api from '../../services/api'
 
-import './styles.css';
+import './styles.css'
 
-import logoImg from '../../assets/logo.svg';
+import logoImg from '../../assets/logo.svg'
 
 export default function Register() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [whatsapp, setWhatsapp] = useState('');
-  const [city, setCity] = useState('');
-  const [uf, setUf] = useState('');
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [whatsapp, setWhatsapp] = useState('')
+  const [city, setCity] = useState('')
+  const [uf, setUf] = useState('')
 
-  const history = useHistory();
+  const history = useHistory()
 
   async function handleResgister(e) {
-    e.preventDefault();
+    e.preventDefault()
 
     const data = {
       name,
@@ -26,14 +26,14 @@ export default function Register() {
       whatsapp,
       city,
       uf,
-    };
+    }
 
     try {
-      const response = api.post('ongs', data);
+      const response = api.post('ongs', data)
 
-      alert(`Seu ID de acesso: ${(await response).data.id}`);
+      alert(`Seu ID de acesso: ${(await response).data.id}`)
 
-      history.push('/');
+      history.push('/')
     } catch (err) {
       alert('Erro no cadastro, tente novamente.')
     }
@@ -43,56 +43,60 @@ export default function Register() {
     <div className="register-container">
       <div className="content">
         <section>
-          <img src={logoImg} alt="Be The Hero"/>
+          <img src={logoImg} alt="Be The Hero" />
 
           <h1>Cadastro</h1>
-          <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem
+            os casos da sua ONG.
+          </p>
 
           <Link className="back-link" to="/">
-          <FiArrowLeft size={16} color="#E02041" />
+            <FiArrowLeft size={16} color="#E02041" />
             Não tenho cadastro
           </Link>
         </section>
 
         <form onSubmit={handleResgister}>
-          <input 
-            placeholder="Nome da ONG" 
-            value={name} 
-            onChange={e => setName(e.target.value)} 
+          <input
+            placeholder="Nome da ONG"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
 
           <input
             type="email"
             placeholder="E-mail"
             value={email}
-            onChange={e => setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
           />
 
-          <input 
+          <input
             placeholder="Whatsapp"
             value={whatsapp}
-            onChange={e => setWhatsapp(e.target.value)}
+            onChange={(e) => setWhatsapp(e.target.value)}
           />
-            
-          <div className="input-group">              
-            <input 
-              placeholder="Cidade" 
+
+          <div className="input-group">
+            <input
+              placeholder="Cidade"
               value={city}
-              onChange={e => setCity(e.target.value)}
+              onChange={(e) => setCity(e.target.value)}
             />
 
-            <input 
+            <input
               placeholder="UF"
-              style={{ width: 80 }} 
+              style={{ width: 80 }}
               value={uf}
-              onChange={e => setUf(e.target.value)}
+              onChange={(e) => setUf(e.target.value)}
             />
-
           </div>
 
-          <button className="button" type="submit">Cadastrar</button>
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
         </form>
       </div>
     </div>
-  );
+  )
 }
